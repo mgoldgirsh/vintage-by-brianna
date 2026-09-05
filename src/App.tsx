40f620c8e524765b3a54719e7f5a1436d45d52d8
Matt Goldgirsh <mgoldgirsh@Matts-MacBook-Pro.local>
2026-09-05 12:53:38 -0400

    revert to items.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import TopBar from './components/TopBar'
 import ClothingSpotlight from './components/ClothingSpotlight'
 import AboutSection from './components/AboutSection'
-import items from './data/items.ts'
+import items from './data/items.json'
+import type { ClothingItem } from './components/ClothingSpotlight'
 import './App.css'
 
 function App() {
   return (
     <>
       <TopBar />
-      <ClothingSpotlight items={items} />
+      <ClothingSpotlight items={items as ClothingItem[]} />
       <AboutSection />
     </>
   )
